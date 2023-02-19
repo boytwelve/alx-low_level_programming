6901b07a5cf46eb8513d6ae1fa70149fd093128d
boytwelve <jeffreydedon@yahoo.com>
2023-02-19 13:34:54 -0800

    n

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print alpha in lower case
+ * Return: void
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
