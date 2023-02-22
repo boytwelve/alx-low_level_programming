8f771e61719ef85db72fc588fc49a113d28cb60b
boytwelve <jeffreydedon@yahoo.com>
2023-02-21 23:52:09 -0800

    l

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowecase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'a'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
